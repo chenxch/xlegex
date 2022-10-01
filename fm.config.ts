c2f5a15f30afb5f9ffab1dbd27e4c2f24ba8a586
Xc <124118265@qq.com>
2022-10-02 00:28:47 +0800

    upd

---
+++ fm.config.ts
@@ -0,0 +1,32 @@
+import { defineFmConfig } from 'fast-imagemin-cli/support'
+
+export default defineFmConfig({
+  include: ['./src/assets'],
+  options: {
+    gifsicle: {
+      optimizationLevel: 7,
+      interlaced: false,
+    },
+    optipng: {
+      optimizationLevel: 7,
+    },
+    mozjpeg: {
+      quality: 20,
+    },
+    pngquant: {
+      quality: [0.8, 0.9],
+      speed: 4,
+    },
+    svgo: {
+      plugins: [
+        {
+          name: 'removeViewBox',
+        },
+        {
+          name: 'removeEmptyAttrs',
+          active: false,
+        },
+      ],
+    },
+  },
+})
